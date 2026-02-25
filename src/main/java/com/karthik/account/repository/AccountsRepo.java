1910d8e01b764cba13731da79eac844e96ed4453
Karthikakkireddy <karthikakkireddy5@gmail.com>
2026-02-25 20:14:32 +0530

    Add delete-account API

--- src/main/java/com/karthik/account/repository/AccountsRepo.java
+++ src/main/java/com/karthik/account/repository/AccountsRepo.java
@@ -2,10 +2,17 @@ package com.karthik.account.repository;
 
 import com.karthik.account.domain.Accounts;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 public interface AccountsRepo extends JpaRepository<Accounts, Long>
 {
    Optional<Accounts> findByCustomerId(Long customerId);
+
+
+   @Transactional
+   @Modifying
+   void deleteByCustomerId(Long customerId);
 }
